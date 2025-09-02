c0d1a1f4dce8843f277dc48a5cd85ca31c6307b4
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-02 14:53:59 +0200

    Fix test for Turbopack

--- test/integration/image-optimizer/test/util.ts
+++ test/integration/image-optimizer/test/util.ts
@@ -1439,9 +1439,12 @@ export function runTests(ctx: RunTestsCtx) {
 
   it('should set cache-control to immutable for static images', async () => {
     if (!ctx.isDev) {
-      const filename = 'test'
+      const filename = fs
+        .readdirSync(join(ctx.appDir, '.next/static/media'))
+        .find((f) => /^test\.[0-9a-f]+\.jpg$/.test(f))
+      expect(filename).toBeString()
       const query = {
-        url: `/_next/static/media/${filename}.fab2915d.jpg`,
+        url: `/_next/static/media/${filename}`,
         w: ctx.w,
         q: ctx.q,
       }
@@ -1454,7 +1457,7 @@ export function runTests(ctx: RunTestsCtx) {
       )
       expect(res1.headers.get('Vary')).toBe('Accept')
       expect(res1.headers.get('Content-Disposition')).toBe(
-        `${contentDispositionType}; filename="${filename}.webp"`
+        `${contentDispositionType}; filename="test.webp"`
       )
       await expectWidth(res1, ctx.w)
 
@@ -1466,7 +1469,7 @@ export function runTests(ctx: RunTestsCtx) {
       )
       expect(res2.headers.get('Vary')).toBe('Accept')
       expect(res2.headers.get('Content-Disposition')).toBe(
-        `${contentDispositionType}; filename="${filename}.webp"`
+        `${contentDispositionType}; filename="test.webp"`
       )
       await expectWidth(res2, ctx.w)
     }
